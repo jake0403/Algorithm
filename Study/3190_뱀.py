7a782c65ba09bee6a3f3abb2055f55aaa2cd09b6
이재상 <jake0403@naver.com>
2021-08-13 02:17:18 +0900

    Algorithm TIL

--- Study/3190_뱀.py
+++ Study/3190_뱀.py
@@ -2,54 +2,48 @@ from collections import deque
 import sys
 input = lambda : sys.stdin.readline().strip()
 
-def snake():
-    global q
+def snake(r,c):
+    q = deque()
+    q.append([r,c])
+    board[r][c] = 2
     delta = {
-        "D": [0,1],
-        "L": [0,-1]
+        'D' : [0, 1, 0, -1],
+        'L' : [1, 0, -1, 0]
     }
-    r = c = 1
-    board[r][c] = 1
-    q.append([1,1])
-    dm = [0,1]
+    direction = 0
     cnt = 1
-    for m in moving:
-        n, d = int(m[0]), m[1]
+
+    for i in range(K):
+        n, D = moving[i][0], moving[i][1]
         for i in range(n):
-            nr, nc = r+dm[0], c+dm[1]
-            if 0 < nr <= N and 0 < nc <= N and (not board[nr][nc] or board[nr][nc] == "A"):
+            nr, nc = r+d[0], c+d[1]
+            if 1<=nr<N and 1<=nc<N and board[nr][nc] != 2:
                 q.append([nr,nc])
                 cnt+=1
-                # 뱀이 다음으로 갈 곳이 사과라면
-                if board[nr][nc] == "A":
-                    board[nr][nc] = board[r][c]
-                    board[nr][nc]+=1
-                # 뱀이 다음으로 갈 곳이 빈 칸이라면
-                else:
-                    if q:
-                        e, m = q.popleft()
-                        board[e][m] = 0
+                # 빈칸이라면
+                if not board[nr][nc]:
+                    dr, dc = q.popleft()
+                    board[dr][dc] = 0
+                    board[nr][nc] = 2
+                # 사과일 때
+                elif board[nr][nc] == 1:
+                    board[nr][nc] = 2
             else:
                 return cnt
-            r,c = nr, nc
-        delta[d][0], delta[d][1] = delta[d][1], delta[d][0]
-        dm = [delta[d][0], delta[d][1]]
+        d[0], d[1] = delta[D][1], delta[D][0]
 
 N = int(input())
-# 사과 수, 위치 정보
-A = int(input())
+K = int(input())
+
 board = [[0]*(N+1) for _ in range(N+1)]
-for a in range(A):
-    a, p = map(int, input().split())
-    board[a][p] = "A"
+for i in range(K):
+    r,c = map(int, input().split())
+    board[r][c] = 1
 
-# 뱀이 몸이 있는 곳의 정보
-visited = [[0]*(N+1) for _ in range(N+1)]
+S = int(input())
+moving = []
+for i in range(S):
+    m, d = input().split()
+    moving.append([int(m), d])
 
-# 뱀의 이동횟수, 이동 정보
-M = int(input())
-moving = [list(input().split()) for _ in range(M)]
-
-# 뱀의 몸 길이 정보
-q = deque()
-print(snake())
+print(snake(1,1))
