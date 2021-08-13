803528d51b963c632dedd4800512df4c72161305
이재상 <jake0403@naver.com>
2021-08-14 03:06:17 +0900

    Algorithm TIL

--- Study/3190_뱀.py
+++ Study/3190_뱀.py
@@ -6,31 +6,33 @@ def snake(r,c):
     q = deque()
     q.append([r,c])
     board[r][c] = 2
-    delta = {
-        'D' : [0, 1, 0, -1],
-        'L' : [1, 0, -1, 0]
-    }
-    direction = 0
+    delta = [(0,1),(1,0),(0,-1),(-1,0)]
     cnt = 1
+    d = 0
+    while True:
+        nr, nc = r + delta[d][0], c + delta[d][1]
+        if 1<= nr <=N and 1<= nc <N and board[nr][nc] != 2:
+            if board[nr][nc] == 0:
+                r, c = q.popleft()
+                board[r][c] = 0
+            board[nr][nc] = 2
+            q.append([nr,nc])
+            if cnt in moving:
+                if moving[cnt] == 'L':
+                    if d == 0:
+                        d = 3
+                    else:
+                        d-=1
+                else:
+                    if d == 3:
+                        d = 0
+                    else:
+                        d+=1
+            cnt+=1
+        else:
+            return cnt
+        r, c = nr, nc
 
-    for i in range(K):
-        n, D = moving[i][0], moving[i][1]
-        for i in range(n):
-            nr, nc = r+d[0], c+d[1]
-            if 1<=nr<N and 1<=nc<N and board[nr][nc] != 2:
-                q.append([nr,nc])
-                cnt+=1
-                # 빈칸이라면
-                if not board[nr][nc]:
-                    dr, dc = q.popleft()
-                    board[dr][dc] = 0
-                    board[nr][nc] = 2
-                # 사과일 때
-                elif board[nr][nc] == 1:
-                    board[nr][nc] = 2
-            else:
-                return cnt
-        d[0], d[1] = delta[D][1], delta[D][0]
 
 N = int(input())
 K = int(input())
@@ -40,10 +42,10 @@ for i in range(K):
     r,c = map(int, input().split())
     board[r][c] = 1
 
-S = int(input())
-moving = []
-for i in range(S):
+L = int(input())
+moving = {}
+for i in range(L):
     m, d = input().split()
-    moving.append([int(m), d])
+    moving[int(m)] = d
 
 print(snake(1,1))
