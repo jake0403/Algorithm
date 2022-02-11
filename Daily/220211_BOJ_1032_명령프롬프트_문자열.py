e847ca3ca7a7ad94c51725c012e67303f3d64319
JAESANG LEE <jake0403@naver.com>
2022-02-12 00:09:47 +0900

    Daily PS adjust

--- Daily/220211_BOJ_1032_명령프롬프트_문자열.py
+++ Daily/220211_BOJ_1032_명령프롬프트_문자열.py
@@ -4,10 +4,10 @@ input = lambda : sys.stdin.readline().strip()
 N = int(input())
 arr = [[] for _ in range(N)]
 for i in range(N):
-    arr[i].append(list(input()))
+    arr[i] = list(input())
 
 tmp = arr[0]
-for i in range(N):
+for i in range(1,N):
     for j in range(len(tmp)):
         if tmp[j] != arr[i][j]:
             tmp[j] = '?'
